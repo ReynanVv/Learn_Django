0726a734a162421109c04fba881f85aeb106cf98
Reynan Vieira <102336632+ReynanVv@users.noreply.github.com>
2023-03-29 16:52:54 -0300

    BD mostrado na tela, utilização de Bootstrap

--- University_PostgreSQL/Aplicacoes/Academico/views.py
+++ University_PostgreSQL/Aplicacoes/Academico/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Curso
 
-# Create your views here.
+
+def home(request):
+    """busca todos os dados no bd e retorna os dados em forma de dicionario"""
+    cursos = Curso.objects.all().order_by('nome')
+    return render(request, "gestaoCursos.html", {"cursos": cursos})
